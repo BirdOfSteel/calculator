aca18da1a2054aabe254254ffd3ca01f6ba70303
BirdOfSteel <AmirAziz2001@Gmail.com>
2024-06-26 02:28:38 +0100

    Preparing and starting app
    - Removed unnecessary files
    - Removed starting code for React
    - Created some divs and styles for calculator frame

---
+++ calculator/src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div id="root-div"> 
+      <div id="calculator-div">
+        <p id="calculator-display">
+          123
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
